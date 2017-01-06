00f346fd14203f017e1af4b206cfbf9700b1f44f
Joakim Hove <joakim.hove@gmail.com>
2017-01-06 14:59:52 +0100

    Added some documentation.

--- opm/output/eclipse/EclipseReader.hpp
+++ opm/output/eclipse/EclipseReader.hpp
@@ -14,19 +14,31 @@ namespace Opm {
 
     class EclipseState;
 
-///
-/// \brief load_from_restart_file
-///     Reading from the restart file, information stored under the OPM_XWEL keyword and SOLUTION data is in this method filled into
-///     an instance of a wellstate object and a SimulatorState object.
-/// \param grid
-///     UnstructuredGrid reference
-/// \param pu
-///     PhaseUsage reference
-/// \param simulator_state
-///     An instance of a SimulatorState object
-/// \param wellstate
-///     An instance of a WellState object, with correct size for each of the 5 contained std::vector<double> objects
-///
+    /*
+      Will load solution data and wellstate from the restart file. The
+      map keys should be a map of keyword names and their
+      corresponding dimension object, i.e. to load the state from a
+      simple two phase simulation you would pass:
+
+         keys = {{"PRESSURE" , UnitSystem::measure::pressure},
+                 {"SWAT" , UnitSystem::measure::identity }}
+
+      For a three phase black oil simulation you would add pairs for
+      SGAS, RS and RV. If you ask for keys which are not found in the
+      restart file an exception will be raised, the happens if the
+      size of a vector is wrong.
+
+      The function will consult the InitConfig object in the
+      EclipseState object to determine which file and report step to
+      load.
+
+      The return value is of type 'data::Solution', which is the same
+      container type which is used by the EclipseWriter, but observe
+      that the dim and target elements carry no information:
+
+         - The returned double data has been converted to SI.
+         . The target is unconditionally set to 'RESTART_SOLUTION'
+    */
 
     std::pair< data::Solution, data::Wells >
     load_from_restart_file( const EclipseState&, const std::map<std::string, UnitSystem::measure>& keys,  int numcells );
